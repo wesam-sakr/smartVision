f7d21387d9cd8677ac6a5b7cee552622ed9a8233
muhamedkrbalo <mohamedkrbalo@gmail.com>
2023-01-22 16:03:26 +0200

    new

---
+++ assets/js/settings-init.js
@@ -0,0 +1,12 @@
+
+"use strict"
+var optionSettings = {
+    layout:"wide",
+    background:"dark",
+    color:"blue",
+    header:"static",
+    font:"opensans",
+    textDirection:"ltr",
+    showSettings:"hide",
+};
+new antlerSettings(optionSettings);
